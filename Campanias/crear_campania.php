6e64959675ab0866b4bbbd475fa92488e98fbfa1
Matías Daza Alvarado <matiasdazaa@gmail.com>
2018-02-27 12:32:11 -0300

    Update: Formato de fecha

--- Campanias/crear_campania.php
+++ Campanias/crear_campania.php
@@ -7,8 +7,38 @@ if (isset($_POST['enviar']))
         $nombrecampania=$_POST['nombrecampania'];
         $FechaInicio=$_POST['FechaInicio'];
         $FechaFin=$_POST['FechaFin'];
-        $nombre= "{$nombrecampania} | {$FechaInicio} | {$FechaFin}";
 
+        $con = new mysqli($servidor, $usuario, $password, $bd);
+        $con->set_charset("utf8");
+        global $con;
+        $sql = "SELECT concat(DATE_FORMAT('$FechaInicio', '%b'), DATE_FORMAT('$FechaInicio', '%y')) as concat";
+        $respuesta = $con -> query($sql);
+        $filas = mysqli_num_rows($respuesta);
+        if($filas > 0)
+        {
+            while($result = $respuesta -> fetch_assoc()) //fetch_assoc() = devuelve un arreglo asociativo con el row en el que se encuentre
+          {
+                $FechaInicioCorta =  $result['concat'];
+
+            }
+        }
+
+        $con = new mysqli($servidor, $usuario, $password, $bd);
+        $con->set_charset("utf8");
+        global $con;
+        $sql = "SELECT concat(DATE_FORMAT('$FechaFin', '%b'), DATE_FORMAT('$FechaFin', '%y')) as concat";
+        $respuesta = $con -> query($sql);
+        $filas = mysqli_num_rows($respuesta);
+        if($filas > 0)
+        {
+            while($result = $respuesta -> fetch_assoc()) //fetch_assoc() = devuelve un arreglo asociativo con el row en el que se encuentre
+          {
+                $FechaFinCorta =  $result['concat'];
+
+            }
+        }
+
+        $nombre= "{$nombrecampania} | {$FechaInicioCorta} | {$FechaFinCorta}";
 
         $sql = "INSERT INTO campania(CAMP_NOMBRE,	CAMP_FINICIO,	CAMP_FFIN, CAMP_NOMBRECAMPANIA)
                     VALUES  ('$nombrecampania','$FechaInicio','$FechaFin', '$nombre')";
@@ -23,5 +53,6 @@ if (isset($_POST['enviar']))
         {
             echo 'Error: '.mysqli_error($con);
         }
+
 }
 ?>
