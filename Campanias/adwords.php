1bbbe3b9d34af89fe94c3863e7e422dcad4408a8
Matías Daza Alvarado <matiasdazaa@gmail.com>
2018-03-02 16:09:49 -0300

    Fix: Error al mostrar campañas filtradas por categoría

--- Campanias/adwords.php
+++ Campanias/adwords.php
@@ -137,7 +137,7 @@
 					 $con = new mysqli($servidor, $usuario, $password, $bd);
 					 $con->set_charset("utf8");
 						 global $con;
-						 $sql = "SELECT * FROM campania group by camp_id desc;";
+						 $sql = "SELECT * FROM campania where CAMP_CATEGORIA = 4 group by camp_id desc;";
 						 $respuesta = $con -> query($sql);
 						 $filas = mysqli_num_rows($respuesta);
 					 ?>
